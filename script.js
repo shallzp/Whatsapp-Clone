741d441e1793633ee88b89cdca4761a2c5cf4b30
shallzp <shalllini0504@gmail.com>
2024-06-21 16:52:52 +0530

    Add files

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
-$(".heading .btns .btn-bg").hover(() => {
-    $(this).css("background-color", "#E6E7E8");
+$(document).ready(() => {
+    clearMaster();
+    $(".chat-room").show();
+    setupNavigationFiltering();
+
+
+    // $(window).scroll(() => {
+    //     if (window.scrollY > 5) {
+    //         $("#header").addClass("black-bg");
+    //     } 
+    //     else {
+    //         $("#header").removeClass("black-bg");
+    //     }
+    // });
 });
+
+$("#master .heading img").each(function(index) {
+    $(this).click(function() {
+        if (index === 1) {
+            $(".header .menu").toggle();
+        } 
+        $(this).toggleClass("clicked");
+    });
+});
+
+
+$("#master .chat-room .chat").each(function() {
+    $(this).on("contextmenu", function(event) {
+        event.preventDefault();
+        $(".chat .menu.contact").show();
+    });
+});
+
+
+
+
+// Navigation
+function setActiveNavItem(activeItem) {
+    $(".nav-items").removeClass("active");
+    $(activeItem).addClass("active");
+}
+
+function setupNavigationFiltering() {
+    const navItems = $('.nav-items');
+
+    navItems.each(function() {
+        $(this).on('click', function() {
+            const itemName = $(this).attr('name');
+            setActiveNavItem(this);
+            clearMaster();
+            $('.' + itemName).show();
+        });
+    });
+}
+
+function filterContent(name) {
+    if(name === "chat-room"){
+        
+    }
+    else if(name === "tv-shows") {
+    
+    }
+    else if(name === "movies") {
+        
+    }
+    else if(name === "new-popular") {
+        
+    }
+    else if(name === "my-list") {
+        
+    }
+    else if (name === "languages") {
+        
+    }
+
+    // window.location.hash = "home-page" + '?' + category ;
+}
+
+function clearMaster() {
+    $(".chat-room").hide();
+    $(".community").hide();
+    $(".status").hide();
+    $(".channel").hide();
+    $(".settings").hide();
+    $(".profile").hide();
+}
