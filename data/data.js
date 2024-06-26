da8133f88aee3a04578219c69ff084c6b6882ab3
shallzp <shalllini0504@gmail.com>
2024-06-26 17:48:24 +0530

    Add fies

--- data/data.js
+++ data/data.js
@@ -1,44 +1,130 @@
-whatsapp_log = {
-    users : [{
-        phone_no : "",
-        name : "",
-        contacts : [{
-            name : "",
-            phone_no : "",
-            chat_log : [{
-                date : "",
-                time : "",
-                msg : "",
-                type : "",
-                attachments : ""
+const whatsapp_log = {
+    users: [{
+        phone_no: "",
+        name: "Shalini",
+        about: "Busy",
+        profile_pic: "./images/data/shalini.png",
+        contacts: [{
+            name: "Sahil",
+            phone_no: "",
+            profile_pic: "./data/profiles/sahil.png",
+            chat_log: [{
+                date: "26-06-2024",
+                time: "10:18 AM",
+                msg: "Good Morning",
+                type: "sent",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:18 AM",
+                msg: "Good Morning",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:19 AM",
+                msg: "What are you doing?",
+                type: "sent",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:20 AM",
+                msg: "Just woke up",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:20 AM",
+                msg: "WBU?",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:21 AM",
+                msg: "Eating Breakfast",
+                type: "sent",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:22 AM",
+                msg: "Okay",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:23 AM",
+                msg: "What are you eating?",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:24 AM",
+                msg: "Parathas",
+                type: "sent",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:24 AM",
+                msg: "Okay Bye I've gotta go",
+                type: "sent",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "10:18 AM",
+                msg: "Bye",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
+            },
+            {
+                date: "26-06-2024",
+                time: "16:57 PM",
+                msg: "Students interested in joining the Student Council should fill out the form by 11 a.m. on June 21, 2024. Interviews for shortlisted candidates will be held on June 22, 2024.",
+                type: "recieved",
+                attachments: false,
+                situation: "read"
             }],
-            call_log : [{
-                date: "",
-                time : "",
-                duration : "",
-                call_type : "",
+            call_log: [{
+                date: "26-06-2024",
+                time: "12:09 AM",
+                duration: "00:59",
+                call_type: "voice"
             }]
         }],
-    },
-    {
-        phone_no : "",
-        name : "",
-        contacts : [{
-            name : "",
-            phone_no : "",
-            chat_log : [{
-                date : "",
-                time : "",
-                msg : "",
-                type : "",
-                attachments : ""
+        groups: [{
+            name: "OFFICIAL MBATECH - BTECH",
+            profile_pic: false,
+            contacts: [],
+            chat_log: [{
+                date: "26-06-2024",
+                time: "10:18 AM",
+                msg: "Good Morning",
+                type: "sent",
+                attachments: false,
+                situation: "unread",
+                by: ""
             }],
-            call_log : [{
-                date: "",
-                time : "",
-                duration : "",
-                call_type : "",
-            }]
-        }],
+            call_log: []
+        }]
     }]
 };
--- script.js
+++ script.js
@@ -1,33 +1,16 @@
+my_profile = whatsapp_log.users[0];
+
 $(document).ready(() => {
     clearMaster();
     $(".chat-room").show();
     setupNavigationFiltering();
+
+    defaultDetails();
+    
+    masterChatLogs();
 });
 
 
-$("#master .heading img").each(function(index) {
-    $(this).click(function() {
-        if (index === 1) {
-            $("#master .header .menu").toggle();
-        } 
-        $(this).toggleClass("clicked");
-    });
-});
-
-
-$("#master .chat-room .chat .open").each(function() {
-    $(this).on("click", function() {
-        $(".chat .menu-contact").toggle();
-    });
-});
-
-
-$("#master .chat-room .chat").each(function() {
-    $(this).on("click", function() {
-        $("#details .default").hide();
-        $("#details .chat-screen").show();
-    });
-});
 
 
 
@@ -83,19 +66,230 @@ function clearMaster() {
 
 
 
+//Master
+$("#master .heading img").each(function(index) {
+    $(this).click(function() {
+        if (index === 1) {
+            $("#master .header .menu").toggle();
+        } 
+        $(this).toggleClass("clicked");
+    });
+});
+
+function createChatListHTML(chat, type, index) {
+    const chatlogs = chat.chat_log;
+    const dataIndex = type === 'group' ? `group-index="${index}"` : `contact-index="${index}"`;
+
+    return `
+    <div class="chat ${type}" ${dataIndex}>
+        ${chat.profile_pic ? 
+            `<div class="profile-pic" style="background-image: url(${chat.profile_pic}); background-repeat: no-repeat; background-size: cover;"></div>` :
+            `<div class="profile-pic user">
+                <img src="./images/icons/${type === 'group' ? 'group-user' : 'user'}.png" class="user">
+            </div>`
+        }
+        <div class="preview">
+            <div class="content">
+                <div class="info">
+                    <div class="top">
+                        <h3 class="contact-name">${chat.name}</h3>
+                        <div class="date-time">${chatlogs[chatlogs.length - 1].date}</div>
+                    </div>
+                    ${chatlogs[chatlogs.length - 1].type === 'sent' ? 
+                        `<img src="./images/icons/${chatlogs[chatlogs.length - 1].situation}.png" class="tick">
+                        <p> ${chatlogs[chatlogs.length - 1].msg.length > 35 ? chatlogs[chatlogs.length - 1].msg.substring(0, 35) + "..." : chatlogs[chatlogs.length - 1].msg} </p>` :
+                        `<p style="margin: 0;">${chatlogs[chatlogs.length - 1].msg.length > 35 ? chatlogs[chatlogs.length - 1].msg.substring(0, 35) + "..." : chatlogs[chatlogs.length - 1].msg}</p>`
+                    }                    
+                    <img src="./images/icons/open.png" class="open-menu">
+                </div>
+            </div>
+            <div class="horizontal-divider default-bg"></div>
+        </div>
+        <div class="menu ${type} white-bg">
+            <ul>
+                ${type === 'group' ? `
+                <li>Archive chat</li>
+                <li>Mute notifications</li>
+                <li>Exit group</li>
+                ` : `
+                <li>Delete chat</li>
+                <li>Block</li>
+                `}
+                <li>Pin chat</li>
+                <li>Mark as unread</li>
+            </ul>
+        </div>
+    </div>
+    `;
+}
+
+
+function masterChatLogs() {
+    const groups = my_profile.groups.map((group, index) => createChatListHTML(group, 'group', index));
+    const contacts = my_profile.contacts.map((contact, index) => createChatListHTML(contact, 'contact', index));
+    
+    $(".chat-list").append(groups.join('') + contacts.join(''));
+
+    $("#master .chat-room .chat").on('click', function() {
+        if ($(this).attr("group-index") !== undefined) {
+            DetailsChange("group", parseInt($(this).attr("group-index")));
+        } 
+        else if ($(this).attr("contact-index") !== undefined) {
+            DetailsChange("contact", parseInt($(this).attr("contact-index")));
+        }
+    });
+}
+
+$("#master").on('click', '.chat-room .chat .open-menu', function() {
+    $(this).closest(".chat").find(".menu").toggle();
+});
+
+
+
+
+
 //Details
+function defaultDetails() {
+    defaultHTML = `
+    <div class="default">
+        <img src="./images/default.png">
+        <h1>Download WhatsApp for Windows</h1>
+        <p>Make calls, share your screen and get a faster experience when you download the Windows app.</p>
+        <button><span>Get from Miscrosoft Store</span></button>
+        <p><img src="./images/icons/lock.png">Your personal messages are end-to-end encrypted</p>
+    </div>
+    `;
 
-//Header
-$(".search-chat").on("click", function() {
-    $(this).toggleClass("clicked");
-    $(".search-tab").show();
-    $("#details").css("width", "calc((100vw - 29vw - 60px)/2 + 42px)");
-});
+    $("#details").append(defaultHTML);
+}
 
-$(".chat-screen .header .menu-btn").click(function() {
-    $(".chat-screen .header .menu").toggle();
-    $(this).toggleClass("clicked");
-});
+function DetailsChange(type, index) {
+    $("#details").empty();
+
+    let data;
+    if (type === 'group') {
+        data = my_profile.groups[index];
+    } else if (type === 'contact') {
+        data = my_profile.contacts[index];
+    }
+
+    $("#details").html(createChatHTML(data, type));
+
+    $(".search-chat").on("click", function() {
+        $(this).toggleClass("clicked");
+        $(".search-tab").show();
+        $("#details").css("width", "calc((100vw - 29vw - 60px)/2 + 42px)");
+    });
+
+    $(".chat-screen .header .menu-btn").click(function() {
+        $(".chat-screen .header .menu").toggle();
+        $(this).toggleClass("clicked");
+    });
+
+    initializeCall(data);
+}
+
+function createMessageHTML(log, chat_type) {
+    const situationImg = log.situation ? `<img src="./images/icons/${log.situation}.png">` : '';
+
+    return `
+    <div class="msg-row ${log.type}">
+        <div class="msg ${log.type}">
+            <p class="msg-text">${log.msg}</p>
+            <div class="right-bottom">
+                <span class="time">${log.time}</span>
+                ${log.type === 'sent' ? situationImg : ''}
+            </div>
+            <div class="img-hover menu-hover">
+                <img src="./images/icons/down.png">
+            </div>
+            <div class="img-hover emoji-hover">
+                <img src="./images/icons/reaction.png">
+            </div>
+            <div class="menu white-bg">
+                <ul>
+                    <li>${log.type === 'sent' ? 'Message info' : ''}</li>
+                    <li>Reply</li>
+                    <li>React</li>
+                    <li>Forward</li>
+                    <li>Pin</li>
+                    <li>Star</li>
+                    <li>${log.type === 'recieved' ? 'Report' : ''}</li>
+                    <li>Delete</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+const chat_date = "";
+function createChatHTML(chat, type) {
+    const chatlogs = chat.chat_log.map(log => createMessageHTML(log)).join('');
+
+    return `
+    <div class="chat-screen">
+        <div class="header default-bg">
+            <div class="left">
+                ${chat.profile_pic ? 
+                    `<div class="profile-pic" style="background-image: url(${chat.profile_pic}); background-repeat: no-repeat; background-size: cover;"></div>` :
+                    `<div class="profile-pic">
+                        <img src="./images/icons/${type === 'group' ? 'group-user' : 'user'}.png" class="user">
+                    </div>`
+                }
+                <h3 class="contact-name">${chat.name}</h3>
+            </div>
+            <div class="right">
+                <button>
+                    <img src="./images/icons/video.png" onclick="startVideoCall()">
+                    <img src="./images/icons/voice.png" onclick="startVoiceCall()">
+                </button>
+                <div class="btns">
+                    <img src="./images/icons/search.png" class="search-chat">
+                    <img src="./images/icons/menu.png" class="menu-btn">
+                </div>
+                <div class="menu white-bg">
+                    <ul>
+                        <li>Contact info</li>
+                        <li>Select messages</li>
+                        <li>Close chat</li>
+                        <li>Mute notifications</li>
+                        <li>Disappearing messages</li>
+                        <li>Clear chat</li>
+                        <li>Delete chat</li>
+                        <li>Report</li>
+                        <li>Block</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="msg-box">
+            <div class="date-day on-top">
+                <h4>${chat_date}</h4>
+            </div>
+            <div class="date-day">
+                <h4>${chat_date}</h4>
+            </div>
+            ${chatlogs}
+        </div>
+
+        <div class="footer default-bg">
+            <div class="btns">
+                <img src="./images/icons/emoji.png">
+                <img src="./images/icons/attach.png">
+            </div>
+            <div class="type-msg">
+                <input type="text" placeholder="Type a message" class="white-bg">
+            </div>
+            <div class="btns">
+                <img src="./images/icons/mic.png">
+            </div>
+        </div>
+    </div>
+    `;
+}
+
 
 //Msg box
 $(".msg-row .menu-hover").each(function() {
@@ -107,11 +301,33 @@ $(".msg-row .menu-hover").each(function() {
 
 
 //Call
+function initializeCall(chat) {
+    const bgHTML = `
+    ${chat.profile_pic ? 
+        `<div class="profile-pic" style="background-image: url(${chat.profile_pic}); background-repeat: no-repeat; background-size: cover;"></div>` :
+        `<div class="profile-pic">
+            <img src="./images/icons/${type === 'group' ? 'group-user' : 'user'}.png" class="user">
+        </div>`
+    }
+
+    <p class="name">${chat.name}</p>
+    <p class="current-msg">Ringing...</p>
+
+    <div class="vedio-screen" style="display: none;">
+        <video id="webcam" autoplay></video>
+    </div>
+    `;
+
+    $(".call .bg").append(bgHTML);
+}
+
 function startVoiceCall() {
     $(".call").show();
 }
 
 function startVideoCall() {
+    $(".vedio-screen").show();
+
     $(".call").show();
 
     let accessDevice = navigator.mediaDevices;
@@ -140,3 +356,14 @@ function startVideoCall() {
         console.log(e.name + ": " + e.message);
     });
 }
+
+$(".call .header .close").click(function() {
+    close($(this).closest(".call"));
+});
+
+
+//For closing extra windows
+function close(element) {
+    // $(element).empty();
+    $(element).hide();
+};
